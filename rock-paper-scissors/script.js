40670189c43e1101bdfa67e7a247021ae687b892
Herr Pinguinito <176357385+DerPinguinito@users.noreply.github.com>
2024-07-24 14:14:32 -0400

    Adds reset button and displays results of each round
    The button resets scores and wipes the display of the round section.
    The round div displays information, such as round numbers and the draw
    selected by both player and computer, whilst also mentioning whether the
    round resulted in a draw, win or loss.

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,5 +1,6 @@
 // this section deals with game functionality
 
+let roundNumber = 0;
 let userScore = 0;
 let computerScore = 0;
 
@@ -40,16 +41,22 @@ function assessOutcome(playerChoice, computerChoice) {
 };
 
 
-function processOutcome(outcome) {
+function updateScores() {
+    PLAYER_SCORE.textContent = `Player: ${userScore}`;
+    COMPUTER_SCORE.textContent = `Computer: ${computerScore}`;
+}
+
+
+function addScores(outcome) {
     
     switch(true) {
         case (outcome === "winner"):
             userScore++;
-            PLAYER_SCORE.textContent = userScore;
+            updateScores();
             break;
         case (outcome === "loser"):
             computerScore++;
-            COMPUTER_SCORE.textContent = computerScore;
+            updateScores;
             break;
         default:
             break;
@@ -58,15 +65,59 @@ function processOutcome(outcome) {
 
 
 function checkIfWinner() {
-    pass;
+    
+    if (userScore < 5 && computerScore < 5) {
+        return;
+    } else if (userScore === 5) {
+        null // if user is the winner
+    } else {
+        null // if computer is the winner
+    }
+    
+};
+
+
+
+function updateRoundNumber() {
+    roundNumber++;
+    ROUND_NUMBER.textContent = `Round #${roundNumber}`;
+};
+
+
+function displayOutcome(playerChoice, computerChoice, outcome) {
+    const OUTPUT = `Player draws ${playerChoice}. Computer draws ${computerChoice}. `;
+    
+    switch(true) {
+        case (outcome === "winner"):
+            DRAWS.textContent = OUTPUT + "You win!";
+            break;
+        case (outcome === "loser"):
+            DRAWS.textContent = OUTPUT + "You lose!";
+            break;
+        default:
+            DRAWS.textContent = OUTPUT + "It's a draw";
+            break;
+    };
 };
 
 
 function play(userInput) {
+    updateRoundNumber();
     const playerChoice = userInput;
     const computerChoice = getComputerChoice();
     const outcome = assessOutcome(playerChoice, computerChoice);
-    processOutcome(outcome);
+    displayOutcome(playerChoice, computerChoice, outcome);
+    addScores(outcome);
+};
+
+
+function resetGame() {
+    roundNumber = 0;
+    userScore = 0;
+    computerScore = 0;
+    updateScores();
+    ROUND_NUMBER.textContent = '';
+    DRAWS.textContent = '';
 };
 
 
@@ -76,29 +127,52 @@ const BODY = document.querySelector("body");
 const GAME_DIV = document.createElement("div");
 const RESULTS_DIV = document.createElement("div");
 const OPTION_DIV = document.createElement("div");
+const ROUND_DIV = document.createElement("div");
 
 BODY.appendChild(GAME_DIV);
 GAME_DIV.appendChild(RESULTS_DIV);
 GAME_DIV.appendChild(OPTION_DIV);
+GAME_DIV.appendChild(ROUND_DIV);
 
 const PLAYER_SCORE = document.createElement("p");
-PLAYER_SCORE.textContent = userScore;
+PLAYER_SCORE.textContent = `Player: ${userScore}`;
 const COMPUTER_SCORE = document.createElement("p");
-COMPUTER_SCORE.textContent = computerScore;
+COMPUTER_SCORE.textContent = `Computer: ${computerScore}`;
 
 RESULTS_DIV.appendChild(PLAYER_SCORE);
 RESULTS_DIV.appendChild(COMPUTER_SCORE);
 
+const ROUND_NUMBER = document.createElement("p");
+
+ROUND_DIV.appendChild(ROUND_NUMBER);
+
+const ROUND_DRAWS = document.createElement("div");
+
+ROUND_DIV.appendChild(ROUND_DRAWS);
+
+const DRAWS = document.createElement("p");
+
+ROUND_DRAWS.appendChild(DRAWS);
+
+
 // this section handles the button creation and their events
 
 function createButtons() {
-    let buttonList = ["rock", "paper", "scissors"];
+    let buttonList = ["rock", "paper", "scissors", "reset"];
     
     buttonList = buttonList.map((button) => {
         const myButton = document.createElement("button");
-        myButton.textContent = button;
-        myButton.setAttribute("id", button);
-        return myButton;
+
+        if (button !== "reset") { 
+            myButton.toggleAttribute("rps");
+            myButton.textContent = button;
+            return myButton;
+
+        } else {
+            myButton.toggleAttribute("reset");
+            myButton.textContent = button;
+            return myButton;
+        };
     });
 
     return buttonList;
@@ -107,9 +181,19 @@ function createButtons() {
 
 function setButtonListenerEvent(buttonObject) {
     
-    buttonObject.addEventListener('click', () => {
-        play(buttonObject.textContent);
-    });
+    if (buttonObject.textContent === "reset") {
+        
+        buttonObject.addEventListener('click', () => {
+            resetGame();
+        });
+
+    } else {
+
+        buttonObject.addEventListener('click', () => {
+            play(buttonObject.textContent);
+        });
+    };
+
     return buttonObject;
 };
 
