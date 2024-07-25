035d84dccf494c1faef3efecb9aa180d8d13f7c8
Herr Pinguinito <176357385+DerPinguinito@users.noreply.github.com>
2024-07-24 20:52:01 -0400

    Creates a 16*16 grid

---
+++ etch-a-sketch/README.md
@@ -0,0 +1,3 @@
+Etch-a-Sketch
+
+The objective of the task is to create an Etch-a-Sketch like game through DOM manipulation. 
---
+++ etch-a-sketch/script.js
@@ -0,0 +1,57 @@
+const BODY = document.querySelector("body");
+const TITLE_DIV = document.createElement("div");
+TITLE_DIV.setAttribute("id", "title-container");
+
+BODY.appendChild(TITLE_DIV);
+
+const TITLE = document.createElement("h1");
+TITLE.setAttribute("id", "title");
+TITLE.textContent = "Etch-a-Sketch";
+
+TITLE_DIV.appendChild(TITLE);
+
+const MAIN = document.createElement("div");
+MAIN.setAttribute("id", "main");
+
+BODY.appendChild(MAIN);
+
+
+function createRows(amountOfRows) {
+    const ROWS = [];
+    
+    for (let i = 0; i < amountOfRows; i++) {
+        const ROW = document.createElement('div');
+        ROW.classList.toggle("row");
+        ROWS.push(ROW);
+    };
+
+    return ROWS
+};
+
+
+function createColumns(amountOfColumns, rowObject) {
+    
+    for (let i = 0; i < amountOfColumns; i++) {
+        const COLUMN = document.createElement("div");
+        COLUMN.classList.toggle("column");
+        COLUMN.textContent = i;
+        rowObject.appendChild(COLUMN);
+    };
+
+    return rowObject
+};
+
+
+function createGrid(rows, columns) {
+    const GRID_DIV = document.createElement("div");
+    GRID_DIV.setAttribute("id", "grid-div");
+
+    let ROWS = createRows(rows);
+
+    ROWS.map((row) => {createColumns(columns, row)});
+    ROWS.forEach(row => {GRID_DIV.appendChild(row)});
+
+    BODY.appendChild(GRID_DIV);
+};
+
+createGrid(16, 16);
