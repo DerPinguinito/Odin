9a44c2cda148c6d48439d5b94bffe7746eb1c8ad
Herr Pinguinito <176357385+DerPinguinito@users.noreply.github.com>
2024-07-25 15:25:45 -0400

    Adds "new grid" button
    The button removes the current grid and replaces it by one of the
    dimensions specified by the user.

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -15,6 +15,38 @@ MAIN.setAttribute("id", "main");
 
 BODY.appendChild(MAIN);
 
+const SIDE_BAR = document.createElement("div");
+SIDE_BAR.classList.toggle("side");
+
+MAIN.appendChild(SIDE_BAR);
+
+const NEW_GRID_BUTTON = document.createElement("button");
+NEW_GRID_BUTTON.textContent = "NEW GRID";
+
+NEW_GRID_BUTTON.addEventListener('click', () => {
+    let userInput = prompt("Introduce a new grid dimension (max value 100)");
+    userInput = parseInt(userInput);
+    validateInput(userInput);
+    removeGrid();
+    createGrid(userInput, userInput);
+
+});
+
+
+SIDE_BAR.appendChild(NEW_GRID_BUTTON);
+
+
+function validateInput(input) {
+    
+    if (isNaN(input)) {
+        alert("Please enter a valid number")
+    } else if (input > 100) {
+        alert("Number cannot be bigger than 100")
+    } else {
+        return;
+    };
+};
+
 
 function createRows(amountOfRows) {
     const ROWS = [];
@@ -59,7 +91,16 @@ function createGrid(rows, columns) {
     ROWS.map((row) => {createColumns(columns, row)});
     ROWS.forEach(row => {GRID_DIV.appendChild(row)});
 
-    BODY.appendChild(GRID_DIV);
+    MAIN.appendChild(GRID_DIV);
 };
 
+
 createGrid(16, 16);
+
+
+function removeGrid() {
+    const GRID = document.getElementById("grid-div");
+    if (GRID) {
+        GRID.parentNode.removeChild(GRID);
+    };
+};
